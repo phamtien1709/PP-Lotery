6402b279155fc5e4d3f3ebf1d9f161bcb4727f87
Tien Pham <tien.phamnguyen@powergatesoftware.com>
2022-08-12 17:35:39 +0700

    Init Lotery

---
+++ Lotery2/app/src/main/java/com/pacificpromise/lotery2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pacificpromise.lotery2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pacificpromise.lotery2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
